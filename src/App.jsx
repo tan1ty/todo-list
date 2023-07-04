009e27f09b857bc707c667a8aed9319830e78d7f
tan1ty <tatiana.kovalenko1992@gmail.com>
2023-07-04 21:11:15 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (index) => {
+    const tasksWithoutDeleted = [...tasks];
+    tasksWithoutDeleted.splice(index, 1);
+    setTasks(tasksWithoutDeleted);
+  };
+
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <form>
+        <input
+          name="new-task"
+          type="text"
+          value={newTask}
+          placeholder="What plans?"
+          onChange={(event) => setNewTask(event.target.value)}
+        />
+        <button type="button" name="add-task" onClick={addTask}>
+          Add task
+        </button>
+      </form>
+      <h2>Tasks list:</h2>
+      <>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <input value={task} />
+              <button
+                type="button"
+                name="delete-task"
+                onClick={() => deleteTask(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+        <h4>You have {tasks.length} tasks</h4>
+      </>
+    </>
+  );
+}
+
+export default App;
